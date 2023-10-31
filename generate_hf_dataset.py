9de0141f6a2c2049c581effd496d819953af2ae2
Edwin Zishiri <edwin.zishiri@yale.edu>
2023-10-30 23:40:34 -0400

    generate hf dataset

--- generate_hf_dataset.py
+++ generate_hf_dataset.py
@@ -1,49 +1,46 @@
-from datasets import DatasetDict, Dataset
-import uuid
+import csv
 
-def read_files_to_dicts(file_paths):
-    output_dicts = []
+def txts_to_csv(txt_file_paths, csv_file_path):
+    row_id = 0  # Initialize row ID counter
 
-    for file_path in file_paths:
-        with open(file_path, 'r') as f:
-            lines = f.readlines()
-        
-        for i in range(0, len(lines), 2):
-            command = lines[i].strip()
-            logical_form = lines[i + 1].strip()
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['id', 'utterance', 'logical_form'])  # Write the header row
 
-            pair_dict = {
-                "command": command,
-                "logical_form": logical_form
-            }
-            output_dicts.append(pair_dict)
+        for txt_file_path in txt_file_paths:
+            with open(txt_file_path, 'r') as txt_file:
+                lines = txt_file.readlines()
+                
+                for i in range(0, len(lines), 2):  # Loop through every pair of lines
+                    utterance = lines[i]
+                    logical_form = lines[i + 1]
+                    csv_writer.writerow([row_id, utterance, logical_form])
+                    row_id += 1  # Increment the row ID for the next pair
 
-    return output_dicts
+train_text_file_paths =["gen/train.txt", "gen_logical/train.txt", "para/train.txt", "para_logical/train.txt"]
+train_csv_file_path = 'train.csv' 
 
-def generate_dataset_dict(file_lists):
-    dataset_splits = {}
-    
-    for split, files in file_lists.items():
-        output_dicts = read_files_to_dicts(files)
 
-        id_translation_list = [{"id": str(uuid.uuid4()), "translation": item} for item in output_dicts]
+test_text_file_paths = ["gen/test.txt", "gen_logical/test.txt", "para/test.txt", "para_logical/test.txt"]
+test_csv_file_path = 'test.csv'
 
-        dataset_splits[split] = Dataset.from_dict({
-            "id": [x['id'] for x in id_translation_list],
-            "translation": [x['translation'] for x in id_translation_list]
-        })
-        
-    return DatasetDict(dataset_splits)
+validation_text_file_paths = ["gen/val.txt", "gen_logical/val.txt", "para/val.txt", "para_logical/val.txt"]
+validation_csv_file_path = 'validation.csv'
 
-if __name__ == "__main__":
-    file_lists = {
-        "train": ["gen/train.txt", "gen_logical/train.txt", "para/train.txt", "para_logical/train.txt"], 
-        "test": ["gen/test.txt", "gen_logical/test.txt", "para/test.txt", "para_logical/test.txt"], 
-        "validation": ["gen/val.txt", "gen_logical/val.txt", "para/val.txt", "para_logical/val.txt"]
-    }
-    
-    dataset_dict = generate_dataset_dict(file_lists)
 
-    # dataset_dict.push_to_hub("ezishiri/robot_commands_dataset") # push your dataset to HF for ease of access 
+# # generate the three csv files 
+# txts_to_csv(train_text_file_paths, train_csv_file_path)
+# txts_to_csv(test_text_file_paths, test_csv_file_path)
+# txts_to_csv(validation_text_file_paths, validation_csv_file_path)
 
-    print(dataset_dict['train'][0]) # make sure things are formatted correctly!
+from datasets import load_dataset
+
+dataset = load_dataset('csv', data_files={
+    'train': 'train.csv',
+    'validation': 'validation.csv',
+    'test': 'test.csv'
+})
+
+dataset.push_to_hub("robot_commands", private=True) # push our dataset to the hf hub 
+
+
